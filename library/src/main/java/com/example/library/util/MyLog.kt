8d369b90817d61391af0436e3ab8f5c07b83e7f7
SH <sh@gmail.com>
2020-12-23 18:42:33 +0530

    Changes in MyLog.kt

--- library/src/main/java/com/example/library/util/MyLog.kt
+++ library/src/main/java/com/example/library/util/MyLog.kt
@@ -1,10 +1,28 @@
 package com.example.library.util
 
+import android.app.Activity
 import android.util.Log
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
 import com.example.library.BuildConfig
 
 
-fun printLog(tag: String, message: String?) {
+fun printLog(tag: String, message: String) {
     if (BuildConfig.DEBUG)
-        message?.let { Log.e(tag, it) }
+        Log.d(tag, message)
+}
+
+fun Activity.printLog(message: String) {
+    if (BuildConfig.DEBUG)
+        printLog(this.javaClass.simpleName, message)
+}
+
+fun Fragment.printLog(message: String) {
+    if (BuildConfig.DEBUG)
+        printLog(this.javaClass.simpleName, message)
+}
+
+fun ViewModel.printLog(message: String) {
+    if (BuildConfig.DEBUG)
+        printLog(this.javaClass.simpleName, message)
 }
